9572c82d5583b4a141fe3c99a314e8496b07b212
Kayran Chaves <99879259+KVChaves@users.noreply.github.com>
2022-02-21 22:20:09 +0000

    Primeira atividade feita.

---
+++ eda01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void maior(int a, int b){
+  int maior2;
+  if (a>b){
+    printf("O maior número é %d",a);
+  }
+  else
+    printf("O maior número é %d",b);
+}
+
+int main() {
+    int valor1, valor2;
+    printf ("digite o valor1 ");
+    scanf("%d", &valor1);
+    printf("digite o valor2 ");
+    scanf("%d", &valor2);
+    
+    maior(valor1, valor2);
+    return 0;
+  }
+  
